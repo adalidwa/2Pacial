d3563812e363ec06b9a052db4af1d7d6d7bc5c8c
adalidwa <adalidjesusxd@gmail.com>
2024-05-10 09:29:25 -0400

    Primer componente

---
+++ src/componente/Informacion.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Informacion() {
+
+    return (
+      <div className="Informacion">
+        <h2>Terapia 2</h2>
+        <h2>Ejecicio pelvicos</h2>
+      </div>
+    )
+  }
+  
+  export default Informacion
